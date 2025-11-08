49737639b97c6a63a0254f101712c31163b13d1b
janmrkonjic <jan.mrkonjic@student.um.si>
2025-11-08 15:20:12 +0100

    spremembe pri imenih datotek in dopolnitev README.md datoteke

--- lekcije_app_be/src/main/java/com/example/lekcije_app_be/LekcijeAppBeApplication.java
+++ lekcije_app_be/src/main/java/com/example/lekcije_app_be/LekcijeAppBeApplication.java
@@ -1,13 +1,13 @@
-package com.example.vaja1be;
+package com.example.lekcije_app_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Vaja1BeApplication {
+public class LekcijeAppBeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Vaja1BeApplication.class, args);
+        SpringApplication.run(LekcijeAppBeApplication.class, args);
     }
 
 }
